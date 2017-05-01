31000f41f92301977107300e22ce5a4f697fc2fb
Michael Stout <stout@uselessopinions.com>
2017-05-01 01:25:06 -0400

    Ship turns

---
+++ Assets/Scripts/Ship.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ship : MonoBehaviour 
+{
+	public float turnSpeed = 100.0f;
+
+	public Bullet bulletPrefab = null;
+	// Use this for initialization
+	void Awake () 
+	{
+		
+	}//Awake
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		float h = Input.GetAxisRaw("Horizontal");
+		transform.eulerAngles += new Vector3(0,0,turnSpeed * Time.deltaTime * h);
+	}//Update
+}//
