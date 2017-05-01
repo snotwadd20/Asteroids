0bf78abc9508fec26ea4c4f6750bd5f4ca39f46a
Michael Stout <stout@uselessopinions.com>
2017-05-01 02:08:50 -0400

    Screen wrapping (copied from https://gamedevelopment.tutsplus.com/articles/create-an-asteroids-like-screen-wrapping-effect-with-unity--gamedev-15055, more or less)

--- Assets/Scripts/Ship.cs
+++ Assets/Scripts/Ship.cs
@@ -32,7 +32,66 @@ public class Ship : MonoBehaviour
 		{
 			Shoot();
 		}//if
+
+		ScreenWrap();
 	}//Update
+
+	bool isWrappingX = false;
+	bool isWrappingY = false;
+
+	void ScreenWrap()
+	{
+		var isVisible = CheckRenderers();
+
+		if(isVisible)
+		{
+			isWrappingX = false;
+			isWrappingY = false;
+			return;
+		}
+
+		if(isWrappingX && isWrappingY) {
+			return;
+		}
+
+		var cam = Camera.main;
+		var viewportPosition = cam.WorldToViewportPoint(transform.position);
+		var newPosition = transform.position;
+
+		if (!isWrappingX && (viewportPosition.x > 1 || viewportPosition.x < 0))
+		{
+			newPosition.x = -newPosition.x;
+
+			isWrappingX = true;
+		}
+
+		if (!isWrappingY && (viewportPosition.y > 1 || viewportPosition.y < 0))
+		{
+			newPosition.y = -newPosition.y;
+
+			isWrappingY = true;
+		}
+
+		transform.position = newPosition;
+	}
+
+	bool CheckRenderers()
+	{
+		Plane[] frustumPlanes = GeometryUtility.CalculateFrustumPlanes(Camera.main);
+
+		foreach(var renderer in GetComponents<Renderer>())
+		{
+
+			// If at least one render is visible, return true
+			if(GeometryUtility.TestPlanesAABB(frustumPlanes, renderer.bounds))
+			{
+				return true;
+			}
+		}
+
+		// Otherwise, the object is invisible
+		return false;
+	}
 
 	void Shoot()
 	{
